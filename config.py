40f902c2c08c8fb4365af9791f72a0f4ad3c2b5e
liqichen <liqichen@me.com>
2019-08-16 14:10:28 +0800

    change column-wise nn

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ class Config(object):
     d_model = 256
     d_ff = 512
     d_row = 60
-    dropout = 0.7
+    dropout = 0.1
     output_size = 4
-    lr = 0.00001
+    lr = 0.0003
     max_epochs = 35
     batch_size = 128
     max_sen_len = 60
