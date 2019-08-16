433a2442f7f2e106c7ee7e59d19207bb1ae9d3a1
liqichen <liqichen@me.com>
2019-08-16 18:20:10 +0800

    learning rate= 0.01

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     d_row = 60
     dropout = 0.1
     output_size = 4
-    lr = 0.01
+    lr = 0.005
     max_epochs = 35
-    batch_size = 32
+    batch_size = 128
     max_sen_len = 60
