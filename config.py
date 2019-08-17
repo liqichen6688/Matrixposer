2265299ab88c25fa4cb3551ac5b0459d3133b5d8
liqichen <liqichen@me.com>
2019-08-17 10:25:46 +0800

    class Config(object):     N = 2     d_model = 256     d_ff = 512     d_row = 60     dropout = 0.2     output_size = 4     lr = 0.001     max_epochs = 35     batch_size = 128     max_sen_len = 60

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 class Config(object):
-    N = 1
+    N = 2
     d_model = 256
     d_ff = 512
     d_row = 60
     dropout = 0.2
     output_size = 4
-    lr = 0.005
+    lr = 0.001
     max_epochs = 35
-    batch_size = 64
+    batch_size = 128
     max_sen_len = 60
