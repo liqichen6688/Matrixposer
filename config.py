81485bdf464d98bd81ebb44d54cda9565ddce8e7
liqichen <liqichen@me.com>
2019-08-17 10:59:49 +0800

    class Config(object):     N = 1     d_model = 512     d_ff = 1028     d_row = 120     dropout = 0.2     output_size = 4     lr = 0.005     max_epochs = 35     batch_size = 32     max_sen_len = 60

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ class Config(object):
     N = 1
     d_model = 512
     d_ff = 1028
-    d_row = 120
+    d_row = 60
     dropout = 0.1
     output_size = 4
     lr = 0.01
     max_epochs = 35
     batch_size = 32
-    max_sen_len = 120
+    max_sen_len = 30
