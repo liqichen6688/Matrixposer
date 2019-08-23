9ac1dedd74eed9811a77459c989580311690ac28
liqichen <liqichen@me.com>
2019-08-23 16:54:20 +0800

    parameter adjust

--- interactor.py
+++ interactor.py
@@ -37,7 +37,7 @@ class Column_wise_nn(nn.Module):
 
 
 class Row_wise_nn(nn.Module):
-    def __init__(self, d_column, d_ff, out_row, dropout=None):
+    def __init__(self, d_column, d_ff, out_row, dropout=None, softmax = False):
         super(Row_wise_nn, self).__init__()
         self.w_1 = nn.Linear(d_column, d_ff)
         self.w_2 = nn.Linear(d_ff, d_ff)
@@ -46,12 +46,13 @@ class Row_wise_nn(nn.Module):
             self.dropout = nn.Dropout(dropout)
         else:
             self.dropout = None
+        self.softmax = softmax
 
-    def forward(self, x, softmax = False):
+    def forward(self, x):
         d_k = x.size(-1)
         output = self.w_2(self.dropout(F.relu(self.w_1(x))))
         output = self.w_3(self.dropout(F.relu(output))) / math.sqrt(d_k)
-        if softmax:
+        if self.softmax:
             output = F.softmax(output, dim=-1)
         if self.dropout is not None:
             output = self.dropout(output)
@@ -69,13 +70,14 @@ class Interactor(nn.Module):
         '''
         super(Interactor, self).__init__()
         self.column_wise_nn = Column_wise_nn(out_row, d_ff, dropout)
-        self.row_wise_nn = Row_wise_nn(d_column, d_ff, out_row, dropout)
+        self.row_wise_nn1 = Row_wise_nn(d_column, d_ff, out_row, dropout)
+        self.row_wise_nn2 = Row_wise_nn(d_column, d_ff, d_column, dropout, softmax=True)
 
     def forward(self, x):
-        left_transposer = self.row_wise_nn(x, True)
+        left_transposer = self.row_wise_nn1(x, True)
         middle_term = torch.matmul(left_transposer.permute(0,2,1), x)
 #        output = self.column_wise_nn(middle_term)
-        output = self.row_wise_nn(middle_term)
+        output = self.row_wise_nn2(middle_term)
         print(output.size())
         #output = torch.matmul(middle_term, right_transposer.permute(0,2,1))
         return output
