9a8c0306691cb75b2e8565a927557fb8e252f660
liqichen <liqichen@me.com>
2019-08-23 16:56:04 +0800

    parameter adjust

--- interactor.py
+++ interactor.py
@@ -74,7 +74,7 @@ class Interactor(nn.Module):
         self.row_wise_nn2 = Row_wise_nn(d_column, d_ff, d_column, dropout, softmax=True)
 
     def forward(self, x):
-        left_transposer = self.row_wise_nn1(x, True)
+        left_transposer = self.row_wise_nn1(x)
         middle_term = torch.matmul(left_transposer.permute(0,2,1), x)
 #        output = self.column_wise_nn(middle_term)
         output = self.row_wise_nn2(middle_term)
