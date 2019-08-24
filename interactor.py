7428b4c37bdabe4ead8ba6f8e54ea9089e133938
liqichen <liqichen@me.com>
2019-08-24 14:36:28 +0800

    add max pool

--- interactor.py
+++ interactor.py
@@ -58,7 +58,7 @@ class Row_wise_nn(nn.Module):
 class ConvPoser(nn.Module):
     def __init__(self, dropout):
         super(ConvPoser, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
