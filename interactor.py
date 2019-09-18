c0621c392af36ae372e8bb557e49137e403a5201
liqichen <liqichen@me.com>
2019-09-18 16:27:24 +0800

    mr

--- interactor.py
+++ interactor.py
@@ -61,6 +61,7 @@ class Row_wise_nn(nn.Module):
 
         return output
 
+
 class Mapper(nn.Module):
     def __init__(self, d_row, d_column, map_size=None,out_row=None):
         super(Mapper, self).__init__()
@@ -74,18 +75,35 @@ class Mapper(nn.Module):
         for _ in range(self.out_row):
             self.all_filters.append(Parameter(torch.rand(d_all_input)/2, requires_grad=True))
 
+        self.indlist = []
+        self.renew_mask()
+
     def forward(self, x):
         x = x.view(-1, self.d_all_input)
         output = []
         for i in range(self.out_row):
-            filter = np.array(self.all_filters[i].tolist())
-            ind = np.argpartition(filter, -self.map_size)[-self.map_size:]
-            ind = ind[np.argsort(filter[ind])].tolist()
+            ind = self.indlist[i]
             one_out = x[:,ind]*self.all_filters[i][ind]
             one_out = one_out.unsqueeze(2)
             output.append(one_out)
         output = torch.cat(output, 2)
         return output
+
+    def renew_mask(self):
+        for i in range(self.out_row):
+            filter = np.array(self.all_filters[i].tolist())
+            ind = np.argpartition(filter, -self.map_size)[-self.map_size:]
+            ind = ind[np.argsort(filter[ind])].tolist()
+            self.indlist.append(ind)
+
+
+    def freeze_parameter(self):
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def unfreeze_parameter(self):
+        for param in self.parameters():
+            param.requires_grad = True
 
 class Interactor(nn.Module):
     def __init__(self, d_column, d_ff, out_row=30, dropout=0.1):
