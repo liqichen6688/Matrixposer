fb7af6b7ed2c4f7667753b2d3b756edc472e55d2
liqichen <liqichen@me.com>
2019-09-18 15:56:20 +0800

    mr

--- interactor.py
+++ interactor.py
@@ -81,7 +81,7 @@ class Mapper(nn.Module):
             filter = np.array(self.all_filters[i].tolist())
             ind = np.argpartition(filter, -self.map_size)[-self.map_size:]
             ind = ind[np.argsort(filter[ind])].tolist()
-            one_out = x[:,ind]*self.all_filters[ind].unsqueeze(2)
+            one_out = x[:,ind]*self.all_filters[i][ind].unsqueeze(2)
             output.append(one_out)
         output = torch.cat(output, 2)
         print(self)
