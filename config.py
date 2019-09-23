13fe94864b37b4442ad77cd3407e8f19334ef5c6
liqichen <liqichen@me.com>
2019-09-23 21:51:20 +0800

    mapper_no_weight

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     output_size = 20
     lr = 0.0001
     max_epochs = 10000000
-    batch_size = 32
+    batch_size = 16
     max_sen_len = 120
     # determine lr
     learning_method = 'reduce'
