49b5db102a87e89b5155991121c91e44502d98d4
liqichen <liqichen@me.com>
2019-10-11 16:39:58 +0800

    pretrain 20ng

--- train.py
+++ train.py
@@ -42,11 +42,11 @@ if __name__=='__main__':
     model.add_optimizer(optimizer)
     model.add_loss_op(Loss)
 
-    if not config.pretrain:
-        model_dict = model.state_dict()
-        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
-        model_dict.update(pretrained_dict)
-        model.load_state_dict(model_dict)
+    #if not config.pretrain:
+    #    model_dict = model.state_dict()
+    #    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    #    model_dict.update(pretrained_dict)
+    #    model.load_state_dict(model_dict)
 
 
 
