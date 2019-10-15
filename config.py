ed8e158a578b0187e19bd6cbb9aef02879f31142
liqichen <liqichen@me.com>
2019-10-15 11:51:09 +0800

    renew pretrain

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     dropout = 0.1
     output_size = 20
     lr = 0.0001
-    max_epochs = 100
+    max_epochs = 2000
     batch_size = 64
     max_sen_len = 30
     # determine lr
