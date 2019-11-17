8173b6a5e569cdeb3d906be776895c9f41cf5d3b
liqichen <liqichen@me.com>
2019-11-17 18:10:00 +0800

    fast

--- model.py
+++ model.py
@@ -61,7 +61,7 @@ class Matposer(nn.Module):
         final_feature_map = encoded_sents
         #final_out = self.fc(final_feature_map)
         #class_out = self.class_fc(final_feature_map[:,-1,:])
-        class_out = self.class_fc(final_feature_map.mean(axis=-2))
+        class_out = self.class_fc(final_feature_map.mean(dim=-2))
         if self.pretrain:
             return final_feature_map
         else:
