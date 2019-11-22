274415e5c459e19775f3ee7595ae5ebc4278587d
liqichen <liqichen@me.com>
2019-11-22 23:14:02 +0800

    fast-translate

--- utils.py
+++ utils.py
@@ -209,9 +209,7 @@ def evaluate_model(model, iterator, is_translate):
             x3_sent = model.dst_embed(x3)
             for i in range(1, x3.shape[1]):
                 output = model.decoder(x3_sent[:, i-1:i], embed_matrix).squeeze(1)
-                print(output.cpu().shape)
                 all_preds.extend(output.cpu().max(1)[1].numpy())
-                print(x3[:, i-1].cpu().shape)
                 all_y.extend(x3[:, i-1].cpu().numpy())
                 right, left = model.matrix_embedding(x3[:, i - 1])
                 embed_matrix = torch.matmul(left, (torch.matmul(embed_matrix, right)))
