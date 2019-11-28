7efba4f8dd6d0cdb1eb5050b276539d831e050ce
liqichen <liqichen@me.com>
2019-11-28 13:35:43 +0800

    fast-translate

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import torch
 
 if __name__=='__main__':
 
-    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     torch.cuda.empty_cache()
     config = Config
     #train_file = '../data/20ng.train'
