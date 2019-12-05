761be039e74ebd1c47544ba910644f5bc15fcaef
liqichen <liqichen@me.com>
2019-12-05 12:38:07 +0800

    fast-translate

--- model.py
+++ model.py
@@ -88,7 +88,7 @@ class Matposer(nn.Module):
         one_hot = torch.zeros_like(pred).scatter(1, gold.view(-1, 1), 1)
         #one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
         log_prb = F.softmax(pred, dim=1)
-        print(log_prb)
+        #print(log_prb)
 
         non_pad_mask = gold.ne(1)
         loss = -(one_hot * log_prb).sum(dim=1)
