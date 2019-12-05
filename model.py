eb317a3fdee6acd35c0930859a05204e1c23dfb6
liqichen <liqichen@me.com>
2019-12-05 13:28:05 +0800

    fast-translate

--- model.py
+++ model.py
@@ -156,7 +156,7 @@ class Matposer(nn.Module):
                     filter = self.matrix_embedding(x3[:, j - 1])
                     info_matrix = F.relu(torch.matmul(filter, embed_matrix))
                     output = self.decoder(x3_sent[:, j-1:j].float(), info_matrix.float()).squeeze(1)
-                    print(output)
+                    #print(output)
                     loss += self.loss_with_smoothing(output, x3[:, j].type(torch.cuda.LongTensor))
                     #loss += self.loss_op(output, x3[:,j].type(torch.cuda.LongTensor))
                     #print(self.loss_op(output, x3[:,j].type(torch.cuda.LongTensor)) / x3.shape[0])
