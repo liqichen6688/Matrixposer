f3c2899d01c3cececc265c7432143a4cf458ba34
liqichen <liqichen@me.com>
2019-12-06 13:05:01 +0800

    fast-translate

--- model.py
+++ model.py
@@ -152,7 +152,7 @@ class Matposer(nn.Module):
                     x3 = x3.type(torch.cuda.LongTensor)
                 loss = 0
                 embed_matrix = self.__call__(x1, x2)
-                x3_sent = self.dst_embed(x3)
+                x3_sent = F.tanh(self.dst_embed(x3))
                 for j in range(1, x3.shape[1]):
                     filter = self.matrix_embedding(x3[:, j - 1])
                     info_matrix = F.relu(torch.matmul(filter, embed_matrix))
