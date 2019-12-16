cda8e0d21c85a01cc58e4119705595f6f09ddf19
liqichen <liqichen@me.com>
2019-12-16 22:35:45 +0800

    fast-translate

--- encoder.py
+++ encoder.py
@@ -63,7 +63,6 @@ class Decoder(nn.Module):
         reattention = torch.sigmoid(torch.matmul(token, past_state_true.permute(0, 2, 1)))
         pre_state = torch.tanh(torch.matmul(reattention, past_state))
         wholeattention = torch.sigmoid(torch.matmul(token, pre_state.permute(0, 2, 1)))
-        print(torch.matmul(reattention, past_state))
         filter_token =  torch.matmul((1 - wholeattention), token) + torch.matmul(wholeattention, pre_state)
         #filter_token = token + pre_state #+ torch.tanh(torch.matmul(x, self.weight) + self.bias)
         return self.dropout(self.out(filter_token))
