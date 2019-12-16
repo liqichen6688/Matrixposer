38d02f41f318d41054eb488ed9dbb8b6a4af3479
liqichen <liqichen@me.com>
2019-12-16 11:16:45 +0800

    fast-translate

--- train_utils.py
+++ train_utils.py
@@ -44,7 +44,7 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model)
+        pe = torch.zeros(max_len, int(d_model))
         position = torch.arange(0, max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() *
                              -(math.log(10000.0) / d_model))
