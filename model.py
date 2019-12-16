b369ea8099bbeb5e7c3a9893f2ef5b868a6f1c91
liqichen <liqichen@me.com>
2019-12-16 12:03:50 +0800

    fast-translate

--- model.py
+++ model.py
@@ -163,7 +163,7 @@ class Matposer(nn.Module):
                 embed_matrix = self.__call__(x1, x2)
                 x3_sent = self.dst_embed(x3)
                 for j in range(1, x3.shape[1]):
-                    info_matrix = torch.tanh(torch.matmul(self.filter(x3[:, j-1:j]), embed_matrix))
+                    info_matrix = torch.tanh(torch.matmul(self.filter(x3[:, j-1]), embed_matrix))
                     print(info_matrix.shape)
 
                     output = self.decoder(self.position2(x3_sent[:, j-1:j].float(), j), info_matrix.float())[:,0,:]
